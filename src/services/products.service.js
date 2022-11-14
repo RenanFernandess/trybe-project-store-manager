51fe91b02cc7c4a07b7c88261b9cacbc50662bdd
Renan Fernandes <renzinestuods@gmail.com>
2022-11-14 19:00:33 -0300

    :bug: fix: Corrige a verificação do if da função getProducts

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -3,7 +3,7 @@ const productsModel = require('../models/products.model');
 const getProducts = async () => {
   const products = await productsModel.getAll();
 
-  if (!products) return { type: 404, message: 'Product not found' };
+  if (!products.length) return { type: 404, message: 'Product not found' };
 
   return {
     type: null,
