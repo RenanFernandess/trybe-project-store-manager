9473d44f6737749b9459939195d85ef96fbc1afe
Renan Fernandes <renzinestuods@gmail.com>
2022-11-14 18:25:13 -0300

    :sparkles: feat: Configura o app. Cria a rota '/products'

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const productsRouter = require('./routes/products.routes');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// iniciando
+app.use('/products', productsRouter);
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação
