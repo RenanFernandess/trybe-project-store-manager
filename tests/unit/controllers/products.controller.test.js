6d5b314f0394fb2fb21cc663e574a9c05f896173
Renan Fernandes <renzinestuods@gmail.com>
2022-11-17 10:12:39 -0300

    :white_check_mark: test: Testa se a função getProducts retorna o status code 200 e os produtos

--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -6,16 +6,29 @@ const { expect } = chai;
 chai.use(sinonChai);
 
 const productsService = require('../../../src/services/products.service');
-const productsController = require('../../../src/services/products.service');
+const productsController = require('../../../src/controllers/products.controller');
 const {
+  products,
   getProductsOk,
   getProductByIdOk,
   nothingFoundError,
 } = require('./mocks/products.controller.mock')
 
-describe('Testando products controller', function () {
-  it('', async function () {
+const STATUS_CODE_OK = 200;
 
+describe('Testando products controller', function () {
+  it('Testa se a função getProducts retorna o status code 200 e os produtos.', async function () {
+    const req = {};
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon.stub(productsService, 'getProducts').resolves(getProductsOk);
+
+    await productsController.getProducts(req, res);
+
+    expect(res.status).to.have.been.calledWith(STATUS_CODE_OK);
+    expect(res.json).to.have.been.calledWith(products);
   });
 
   afterEach(sinon.restore);
