18493090fe326a5b029dd1ada278332b46633b41
Renan Fernandes <renzinestuods@gmail.com>
2022-11-17 17:45:30 -0300

    :test_tube: test: Cria o mock newProduct

--- tests/unit/controllers/mocks/products.controller.mock.js
+++ tests/unit/controllers/mocks/products.controller.mock.js
@@ -25,9 +25,15 @@ const getProductByIdOk = {
 
 const nothingFoundError = { type: 404, message: 'Product not found' };
 
+const newProduct = {
+  id: 7,
+  name: 'peixe xablau',
+};
+
 module.exports = {
   products,
   getProductsOk,
   getProductByIdOk,
   nothingFoundError,
+  newProduct,
 };
