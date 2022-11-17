7418f2ddc247ec3a8b74f0589fc4fcccc59b67bc
Renan Fernandes <renzinestuods@gmail.com>
2022-11-17 16:05:57 -0300

    :sparkles: feat: Cria a funçã addProduct que verifica se esta tudo certo com os dados do produto, caso esteja tudo certo solicita a inserção do novo produto

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -19,7 +19,20 @@ const getProductById = async (id) => {
   return { type: null, message: product };
 };
 
+const addProduct = async (product) => {
+  const { name } = product;
+  if (!name) return { type: 400, message: '"name" is required' };
+  if (name.length < 5) {
+    return { type: 422, message: '"name" length must be at least 5 characters long' };
+  }
+
+  const result = await productsModel.insert(product);
+
+  return { type: null, message: result };
+};
+
 module.exports = {
   getProducts,
   getProductById,
+  addProduct,
 };
