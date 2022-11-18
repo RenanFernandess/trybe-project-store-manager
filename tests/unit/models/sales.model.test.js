d3ed690dac842a85e117c5d8019ab1640ac5da5e
Renan Fernandes <renzinestuods@gmail.com>
2022-11-18 20:04:14 -0300

    :test_tube: test: Testa sales model

---
+++ tests/unit/models/sales.model.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const salesModel = require('../../../src/models/sales.model');
+const connection = require('../../../src/models/db/connection');
+
+const { saleProducts } = require('./mocks/sales.model.mock');
+
+describe('Testa sales model', function () {
+  it('', async function () {
+
+  });
+});
