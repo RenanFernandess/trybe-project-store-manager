41063ecfe99f455bd64822e03ac895aacd53c4ca
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 20:12:00 -0300

    :sparkles: feat: Cria a função getSales

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -8,6 +8,15 @@ const addSale = async ({ body }, res) => {
   return res.status(201).json(message);
 };
 
+const getSales = async (_req, res) => {
+  const { type, message } = await salesService.getSales();
+
+  if (type) return res.status(type).json({ message });
+
+  return res.status(200).json(message);
+};
+
 module.exports = {
   addSale,
+  getSales,
 };
