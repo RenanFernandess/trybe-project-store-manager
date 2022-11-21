90a10828e60d91f09fe2f9523f61fddc0a9264ae
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 18:48:19 -0300

    :white_check_mark: test: Testa se a função addSale retorna status code 201 e os dados da venda

--- tests/unit/controllers/sales.controller.test.js
+++ tests/unit/controllers/sales.controller.test.js
@@ -7,10 +7,21 @@ chai.use(sinonChai);
 
 const salesService = require('../../../src/services/sales.service');
 const salesController = require('../../../src/controllers/sales.controller');
+const { addSaleReturn, porductIdIsReq, saleProducts } = require('./mocks/sales.controller.mock');
 
-describe('Testa sales controller', function () {
-  it('', async function () {
+describe('Testa sales controller', async function () {
+  it('Testa se a função addSale retorna status code 201 e os dados da venda.', async function () {
+    const req = { body: saleProducts };
+    const res = {};
 
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon.stub(salesService, 'addSale').resolves(addSaleReturn);
+
+    const result = await salesController.addSale(req, res);
+
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(addSaleReturn.message);
   });
 
   afterEach(sinon.restore);
