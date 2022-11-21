757d2cc3bd6dfc5d71fc0973787f37ffc88d2ef4
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 11:40:42 -0300

    :test_tube: test: Testa sales service

---
+++ tests/unit/services/sales.service.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const salesModel = require('../../../src/models/sales.model');
+const salesService = require('../../../src/services/sales.service');
+const { saleProducts } = require('./mocks/sales.service.mock');
+
+describe('Testa sales service', function () {
+  it('', async function () {
+
+  })
+
+  afterEach(sinon.restore);
+});
