d6824a15d307610f584fb111b2d224f0c130d1f0
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 12:04:38 -0300

    :white_check_mark: test: Testa se a função addSale retorna os detalher da venda

--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -3,11 +3,17 @@ const sinon = require('sinon');
 
 const salesModel = require('../../../src/models/sales.model');
 const salesService = require('../../../src/services/sales.service');
-const { saleProducts } = require('./mocks/sales.service.mock');
+const { saleProducts, addSaleReturn } = require('./mocks/sales.service.mock');
 
 describe('Testa sales service', function () {
-  it('', async function () {
+  it('Testa se a função addSale retorna os detalher da venda.', async function () {
+    sinon.stub(salesModel, 'insertSale').resolves(1);
+    sinon.stub(salesModel, 'insertSaleProducts').resolves(undefined);
+    sinon.stub(salesModel, 'getSaleDetails').resolves(saleProducts);
 
+    const result = await salesService.addSale(saleProducts);
+
+    expect(result).to.be.deep.equal(addSaleReturn);
   })
 
   afterEach(sinon.restore);
