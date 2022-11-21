05ff7e1cabbdb90eab488df71fc241062043b401
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 13:42:57 -0300

    :white_check_mark: test: Testa se a função retorna status conde e message de error

--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -3,7 +3,8 @@ const sinon = require('sinon');
 
 const salesModel = require('../../../src/models/sales.model');
 const salesService = require('../../../src/services/sales.service');
-const { saleProducts, addSaleReturn } = require('./mocks/sales.service.mock');
+const productsModule = require('../../../src/models/products.model');
+const { saleProducts, addSaleReturn, quantityIsReq } = require('./mocks/sales.service.mock');
 
 describe('Testa sales service', function () {
   it('Testa se a função addSale retorna os detalher da venda.', async function () {
@@ -14,7 +15,24 @@ describe('Testa sales service', function () {
     const result = await salesService.addSale(saleProducts);
 
     expect(result).to.be.deep.equal(addSaleReturn);
-  })
+  });
+
+  it('Testa se a função retorna status conde e message de error.', async function () {
+    const mock =[
+      {
+        "productId": 1,
+      },
+      {
+        "productId": 2,
+        "quantity": 5,
+      }
+    ];
+
+    const result = await salesService.addSale(mock);
+
+    expect(result.type).to.be.equal(quantityIsReq.type);
+    expect(result.message).to.be.equal(quantityIsReq.message);
+  });
 
   afterEach(sinon.restore);
 });
