ba6c6755c245b18ba613e6098c5a63872ea6dacb
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 13:42:19 -0300

    :test_tube: test: adiciona os mocks do errors

--- tests/unit/services/mocks/sales.service.mock.js
+++ tests/unit/services/mocks/sales.service.mock.js
@@ -17,7 +17,18 @@ const addSaleReturn = {
   },
 };
 
+const porductIdIsReq = { type: 400, message: '"productId" is required' };
+const quantityIsReq = { type: 400, message: '"quantity" is required' };
+const quantityValueError = {
+  type: 422, message: '"quantity" must be greater than or equal to 1',
+};
+const productNotFound = { type: 404, message: 'Product not found' }
+
 module.exports = {
   saleProducts,
   addSaleReturn,
+  porductIdIsReq,
+  quantityIsReq,
+  quantityValueError,
+  productNotFound,
 };
