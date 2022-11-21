be553aab9907496aa9f7a9ac4f5bb68770185f97
Renan Fernandes <renzinestuods@gmail.com>
2022-11-21 18:41:05 -0300

    :test_tube: test: Adiciona o mock dos itemsSold

--- tests/unit/controllers/mocks/sales.controller.mock.js
+++ tests/unit/controllers/mocks/sales.controller.mock.js
@@ -1,23 +1,27 @@
+const saleProducts = [
+  {
+    "productId": 1,
+    "quantity": 1
+  },
+  {
+    "productId": 2,
+    "quantity": 5
+  }
+];
+
+
 const addSaleReturn = {
   type: null,
   message: {
     id: 1,
-    itemsSold: [
-      {
-        "productId": 1,
-        "quantity": 1
-      },
-      {
-        "productId": 2,
-        "quantity": 5
-      }
-    ],
+    itemsSold: saleProducts,
   },
 };
 
 const porductIdIsReq = { type: 400, message: '"productId" is required' };
 
 module.exports = {
+  saleProducts,
   addSaleReturn,
   porductIdIsReq,
 };
