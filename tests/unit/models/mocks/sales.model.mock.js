56c6ae1f8d04a89e6a9b933d56acbbdaafb527fe
Renan Fernandes <renzinestuods@gmail.com>
2023-05-19 17:16:07 -0300

    :white_check_mark: test: Testa o arquivo sales.mode.js

--- tests/unit/models/mocks/sales.model.mock.js
+++ tests/unit/models/mocks/sales.model.mock.js
@@ -9,6 +9,25 @@ const saleProducts = [
   }
 ];
 
+const sales = [
+  {
+    "saleId": 1,
+    "date": "2023-05-19T19:30:27.000Z",
+    "productId": 2,
+    "quantity": 10
+  },
+  {
+    "saleId": 2,
+    "date": "2023-05-19T19:30:27.000Z",
+    "productId": 3,
+    "quantity": 15
+  }
+]
+
+const [sale] = sales;
+
 module.exports = {
   saleProducts,
+  sales,
+  sale,
 };
--- tests/unit/models/sales.model.test.js
+++ tests/unit/models/sales.model.test.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 const salesModel = require('../../../src/models/sales.model');
 const connection = require('../../../src/models/db/connection');
 
-const { saleProducts } = require('./mocks/sales.model.mock');
+const { saleProducts, sale, sales } = require('./mocks/sales.model.mock');
 
 describe('Testa sales model', function () {
   it('Testa se a função insertSale retorna o id da venda.', async function () {
@@ -15,6 +15,15 @@ describe('Testa sales model', function () {
     expect(result).to.be.equal(1);
   });
 
+  it('Testa se a função insertSaleProducts funciona corretamente.', async function () {
+    sinon.stub(connection, 'execute').resolves([{ insertId: 1 }]);
+
+    const saleId = 1
+    const result = await salesModel.insertSaleProducts(saleId, { productId: 1, quantity: 1 });
+
+    expect(result).to.be.equal(undefined);
+  });
+
   it('Testa se a função getSaleDetails retorna os detalhes da venda produto e quantidade.', async function () {
     sinon.stub(connection, 'execute').resolves([saleProducts]);
 
@@ -22,6 +31,23 @@ describe('Testa sales model', function () {
     const result = await salesModel.getSaleDetails(saleId);
 
     expect(result).to.be.deep.equal(saleProducts);
+  });
+
+  it('Testa se função getAll tem o retorno esperado.', async function () {
+    sinon.stub(connection, 'execute').resolves([sales]);
+
+    const result = await salesModel.getAll();
+
+    expect(result).to.be.equal(sales);
+  });
+
+  it('Testa se for passado um id valido para a função findById tem o retorno esperado.', async function () {
+    sinon.stub(connection, 'execute').resolves([[sale]]);
+
+    const saleId = 1
+    const result = await salesModel.findById(saleId);
+
+    expect(result).to.be.deep.equal([sale]);
   });
 
   afterEach(sinon.restore);
